3baebe8ab4b62424ce097caa5b88bdfcafe4eebf
Junior Alencar <junioralencar31415@gmail.com>
2024-12-10 13:13:10 -0300

    fix data 2

--- scripts/python/multithread_pc.py
+++ scripts/python/multithread_pc.py
@@ -17,22 +17,23 @@ import pandas as pd
 #return: set of .json file with above parameters 
 
 
-N = 5000
+N = 160000
 dim = 5
 alpha_a = [17.0, 18.0, 19.0, 20.0, 21.0, 22.0]
 alpha_g = 2.0
 N_s = 20000
-# df = pd.read_csv("run_multi.txt", delimiter=' ')
+df = pd.read_csv("run_multi.txt", delimiter=' ')
+df_n = df[df["N"]==N]
 
-# for _, row in df.iterrows():
-#     FunctionsFile.JsonGenerate(int(row["N"]), float(row["alpha_a"]), float(row["alpha_g"]), float(row["dim"]))
-#     FunctionsFile.JsonGenerate(int(row["N"]), float(row["alpha_a"]), float(row["alpha_g"]), float(row["dim"]))    
+for _, row in df_n.iterrows():
+    FunctionsFile.JsonGenerate(int(row["N"]), float(row["alpha_a"]), float(row["alpha_g"]), float(row["dim"]))
+    FunctionsFile.JsonGenerate(int(row["N"]), float(row["alpha_a"]), float(row["alpha_g"]), float(row["dim"]))    
 # for i in range(len(N)):
 #     FunctionsFile.multithread_pc(N[i], N_samples[i])
 #     FunctionsFile.permission_run(N[i])
 
-for i in range(len(alpha_a)):
-    FunctionsFile.JsonGenerate(N, alpha_a[i], alpha_g, dim)
+# for i in range(len(alpha_a)):
+#     FunctionsFile.JsonGenerate(N, alpha_a[i], alpha_g, dim)
 
 FunctionsFile.multithread_pc(N, N_s)
 FunctionsFile.permission_run(N)
